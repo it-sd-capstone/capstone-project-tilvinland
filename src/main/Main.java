feb81ae68a010615e9724122203ec9bfa8109a56
Steven <swilliams51@student.cvtc.edu>
2025-04-18 17:31:42 -0500

    Adds Technology Stack

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,42 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
+    public static final String DATABASE_NAME = "vinland";
+    public static final String DATABASE_PATH = DATABASE_NAME + ".db";
+    private static final int TIMEOUT_STATEMENT_S = 5;
     public static void main(String[] args) {
         System.out.println("Wattup");
+    }
+
+    public static Connection createConnection() {
+        Connection result = null;
+        try {
+            result = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH);
+            Statement command = result.createStatement();
+            command.setQueryTimeout(TIMEOUT_STATEMENT_S);
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ResultSet queryRaw(Connection db, String sql) {
+        ResultSet result = null;
+        try {
+            Statement statement = db.createStatement();
+            result = statement.executeQuery(sql);
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
     }
 }
