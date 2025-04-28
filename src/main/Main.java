b72acb0aac246dc386edfea26b8100834414c92a
Steven <swilliams51@student.cvtc.edu>
2025-04-28 15:16:13 -0500

    Adds Input filter and setSeed()

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,12 +3,18 @@ package main;
 import javax.swing.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.text.AbstractDocument;
+import javax.swing.text.*;
+import javax.swing.text.DocumentFilter;
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.awt.SystemColor.text;
 
 public class Main {
     public static final String DATABASE_NAME = "vinland";
@@ -22,6 +28,39 @@ public class Main {
     static JLabel labelState;
     static JButton buttonAcknowledge;
     private static final String CARD_MAIN = "Main";
+
+    private static Random rng;
+
+    private static class InputFilter extends DocumentFilter {
+        private static final int MAX_LENGTH = 10;
+
+        @Override
+        public void insertString(FilterBypass fb, int offset, String stringToAdd, AttributeSet attr)
+                throws BadLocationException {
+
+            String text = fb.getDocument().getText(0, fb.getDocument().getLength());
+
+            if (fb.getDocument() != null) {
+                if ((fb.getDocument().getLength() + stringToAdd.length()) <= MAX_LENGTH && text.matches("^[a-zA-Z0-9]*$")) {
+                    super.insertString(fb, offset, stringToAdd, attr);
+                }
+
+            } else {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int lengthToDelete, String stringToAdd, AttributeSet attr)
+                throws BadLocationException {
+            if (fb.getDocument() != null) {
+                if ((fb.getDocument().getLength() - lengthToDelete + stringToAdd.length()) <= MAX_LENGTH)
+                    super.replace(fb, offset, lengthToDelete, stringToAdd, attr);
+            } else {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
 
     public static void main(String[] args) {
         // Test if application boots properly
@@ -89,4 +128,13 @@ public class Main {
         }
         return result;
     }
+
+    public static void setSeed(String input) {
+        long seed = (long) input.hashCode();
+        rng = new Random();
+        rng.setSeed(seed);
+
+    }
+
+
 }
